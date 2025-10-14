f1a96517a8fccee144543864dc991d24dbdcd812
matcor-bodc <matcor@noc.ac.uk>
2025-10-14 11:37:32 +0100

    Update demos readme

---
+++ demos/README.md
@@ -0,0 +1,3 @@
+# Example Notebook
+
+To run this notebook, copy the *.ipynb* file along with the *files* subfolder into a Jupyter environment. From there you should be able to run the cells in order to see the results.
--- demos/demo_script.py
+++
@@ -1,6 +0,0 @@
-from argo_metadata_validator.validation import ArgoValidator
-from pprint import pprint
-
-validation_results = ArgoValidator().validate(["files/valid_sensor.json", "files/invalid_sensor.json"])
-
-pprint(validation_results)
