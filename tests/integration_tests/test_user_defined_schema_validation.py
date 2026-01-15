e3906e6ac8613148e3e0230cf6313bb25107469c
Chris Thompson <chromp@noc.ac.uk>
2026-01-15 11:29:40 +0000

    Add tests

---
+++ tests/integration_tests/test_user_defined_schema_validation.py
@@ -0,0 +1,33 @@
+"""Test complete validation process for input files when validated against a user-supplied schema."""
+
+from pathlib import Path
+
+import pytest
+
+from argo_metadata_validator.models.results import ValidationError
+from argo_metadata_validator.validation import ArgoValidator
+
+test_cases = [
+    ["valid_sensor.json", []],
+    [
+        "invalid_sensor.json",
+        [
+            ValidationError(
+                message="'SENSORS' is a required property",
+                path="",
+            ),
+            ValidationError(message="Additional properties are not allowed ('SENSORZ' was unexpected)", path=""),
+        ],
+    ],
+]
+
+
+@pytest.mark.parametrize("file_path,expected_errors", test_cases)
+def test_validating_files_with_user_defined_schema(file_path, expected_errors):
+    """Test that validation works as expected with a user-defined schema."""
+    resolved_file_path = Path(__file__).parent.parent / "files" / "user_defined" / file_path
+    resolved_schema_path = Path(__file__).parent.parent / "files" / "user_defined" / "schema" / "sensor.schema.json"
+
+    errors = ArgoValidator().validate([str(resolved_file_path)], resolved_schema_path)
+
+    assert errors == {file_path: expected_errors}
